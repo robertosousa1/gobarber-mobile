c8a70276cd3574de558d6058c0fadca10ea742f9
Roberto <roberto_junior_sousa@hotmail.com>
2020-01-15 21:22:57 -0300

    recuperando status de logado ou não do redux para informar a função de rotas

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 
-import Routes from './routes';
+import createRouter from './routes';
 
 export default function App() {
+  const signed = useSelector(state => state.auth.signed);
+
+  const Routes = createRouter(signed);
+
   return <Routes />;
 }
