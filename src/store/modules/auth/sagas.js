57eec99fd74b96c056d18fa3da1ccef7f2d20ad3
Roberto <roberto_junior_sousa@hotmail.com>
2020-01-15 21:06:47 -0300

    delay ao realizar login

--- src/store/modules/auth/sagas.js
+++ src/store/modules/auth/sagas.js
@@ -1,5 +1,5 @@
 import { Alert } from 'react-native';
-import { takeLatest, call, put, all } from 'redux-saga/effects';
+import { takeLatest, call, put, all, delay } from 'redux-saga/effects';
 
 import api from '~/services/api';
 
@@ -26,6 +26,8 @@ export function* signIn({ payload }) {
     }
 
     api.defaults.headers.Authorization = `Bearer ${token}`;
+
+    yield delay(500);
 
     yield put(signInSuccess(token, user));
 
