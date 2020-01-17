8158227f32b3478ab0dfce36ec9b39b22250ba6c
Roberto <roberto_junior_sousa@hotmail.com>
2020-01-16 21:50:00 -0300

    criado botão sair no formulário

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -4,6 +4,7 @@ import Icon from 'react-native-vector-icons/MaterialIcons';
 import PropTypes from 'prop-types';
 
 import Background from '~/components/Background';
+import { signOut } from '~/store/modules/auth/actions';
 import { updateProfileRequest } from '~/store/modules/user/actions';
 
 import {
@@ -13,6 +14,7 @@ import {
   FormInput,
   Separator,
   SubmitButton,
+  LogoutButton,
 } from './styles';
 
 export default function Profile() {
@@ -46,6 +48,10 @@ export default function Profile() {
         confirmPassword,
       })
     );
+  }
+
+  async function handleLogout() {
+    dispatch(signOut());
   }
 
   return (
@@ -111,6 +117,7 @@ export default function Profile() {
           />
 
           <SubmitButton onPress={handleSubmit}>Atualizar perfil</SubmitButton>
+          <LogoutButton onPress={handleLogout}>Sair do GoBarber</LogoutButton>
         </Form>
       </Container>
     </Background>
